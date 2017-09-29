fc4bd7daf86263ca2ab2ef5c9245b7f85306fc55
alisoncassisn <alisoncassisn@gmail.com>
2017-09-29 01:31:10 -0300

    alteracoes e packages

--- config/express.js
+++ config/express.js
@@ -1,9 +1,19 @@
 const path = require('path')
+const cors = require('cors')
+const axios = require('axios')
 const express = require('express')
 const app = express()
+const plateUrl = 'http://placa-wgenial.rhcloud.com/'
 
+app.use(cors())
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')))
+
+app.get('/api/plate/:plate', (req, res) => {
+    axios.get(plateUrl + req.params.plate)
+        .then(resp => res.send(resp.data))
+        .catch(err => console.log(err))
+})
 
 module.exports = app
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,29 @@
-var btnBudget = document.querySelector('#btn_budget')
-var fepiModal = document,querySelector('.fepi-modal')
+const btnBudget = document.querySelector('#btn_budget')
+const fepiModal = document.querySelector('.fepi-modal')
+const plate = document.querySelector('#plate')
 
-window.onLoad = function() {
+window.onload = function() {
     btnBudget.onclick = showFIPEModal
+    plate.onchange = searchByPlate
 }
 
 function showFIPEModal() {
     fepiModal.classList.add('show')
+}
+
+function searchByPlate() {
+    const value = plate.value
+    value.replace('-', '')
+    getRequest(`api/plate/${value}`)
+}
+
+function getRequest(route) {
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(req.responseText))
+        }
+    }
+    req.open("GET", route, true)
+    req.send()
 }
